a15da8cbd3eeab826b6240292917f0dc23fdc672
Roland Illig <roland.illig@gmx.de>
2015-10-25 22:38:01 +0100

    Argh, jetzt läuft der Unittest wieder

--- lines_test.go
+++ lines_test.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"fmt"
-	"os"
 	"testing"
 )
 
@@ -12,12 +10,12 @@ type Asserter struct {
 func (a *Asserter) assertStringEqual(expected, actual string) {
 	if actual != expected {
 		a.t.Fatalf("Expected %#v, got %#v", expected, actual)
-	} else {
-		fmt.Printf("assertStringEqual: %#v\n", actual)
 	}
 }
 
 func TestConvertToLogicalLines_nocont(t *testing.T) {
+	GlobalVars.opts = &CmdOpts{}
+
 	a := Asserter{t}
 	phys := []PhysLine{
 		{1, "first line\n"},
@@ -30,10 +28,11 @@ func TestConvertToLogicalLines_nocont(t *testing.T) {
 	a.assertStringEqual("fname", lines[1].fname)
 	a.assertStringEqual("2", lines[1].lines)
 	a.assertStringEqual("second line", lines[1].text)
-	a.assertStringEqual("a","b")
 }
 
 func TestConvertToLogicalLines_contInLastLine(t *testing.T) {
+		GlobalVars.opts = &CmdOpts{}
+
 	a := Asserter{t}
 	physlines := []PhysLine{
 		{1, "last line\\"},
@@ -41,13 +40,5 @@ func TestConvertToLogicalLines_contInLastLine(t *testing.T) {
 	lines := convertToLogicalLines("fname", physlines, true)
 	a.assertStringEqual("fname", lines[0].fname)
 	a.assertStringEqual("1", lines[0].lines)
-	a.assertStringEqual("last line", lines[0].text)
-}
-
-func TestMe(t *testing.T) {
-	fmt.Fprintf(os.Stderr, "hello from test\n")
-}
-
-func TestMain(m *testing.M) {
-	GlobalVars.opts = &CmdOpts{}
+	a.assertStringEqual("last line ", lines[0].text)
 }
