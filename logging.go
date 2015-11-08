36b3f879293ad227ea6294d73ca79109c4a96c45
Roland Illig <roland.illig@gmx.de>
2015-11-08 19:05:36 +0100

    space

--- logging.go
+++ logging.go
@@ -95,7 +95,7 @@ func printSummary() {
 		if G.errors != 0 && G.warnings != 0 {
 			fmt.Printf("%d errors and %d warnings found.", G.errors, G.warnings)
 			if !G.opts.optExplain {
-				fmt.Printf("(Use -e for more details.)")
+				fmt.Printf(" (Use -e for more details.)")
 			}
 			fmt.Printf("\n")
 		} else {
