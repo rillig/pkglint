1f81e4180fec6422bd7416140d7e748101bd3563
Roland Illig <roland.illig@gmx.de>
2015-11-13 20:22:36 +0100

    unit test for patch without comment

--- patches_test.go
+++ patches_test.go
@@ -4,9 +4,9 @@ import (
 	check "gopkg.in/check.v1"
 )
 
-func (s *Suite) TestChecklinesPatch(c *check.C) {
+func (s *Suite) TestChecklinesPatch_WithComment(c *check.C) {
 	lines := mklines("patch-as",
-		"$NetBSD$",
+		"$"+"NetBSD$",
 		"",
 		"Text",
 		"Text",
@@ -22,5 +22,23 @@ func (s *Suite) TestChecklinesPatch(c *check.C) {
 	checklinesPatch(lines)
 
 	c.Check(G.errors, equals, 0)
+	c.Check(G.warnings, equals, 0)
+}
+
+func (s *Suite) TestChecklinesPatch_WithoutComment(c *check.C) {
+	lines := mklines("patch-as",
+		"$"+"NetBSD$",
+		"",
+		"--- file.orig",
+		"+++ file",
+		"@@ -5,3 +5,3 @@",
+		" context before",
+		"-old line",
+		"+old line",
+		" context after")
+
+	checklinesPatch(lines)
+
+	c.Check(G.errors, equals, 1) // patch-as:3: Comment expected
 	c.Check(G.warnings, equals, 0)
 }
