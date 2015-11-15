dfdc9a386af96b4296a5135767eab18b2b8dd807
Roland Illig <roland.illig@gmx.de>
2015-11-15 15:58:00 +0100

    proof SimpleMatch

--- pkglint-all.pl
+++ pkglint-all.pl
@@ -40,50 +40,6 @@ sub log_message { # no prototype due to Perl weirdness
 	}
 }
 
-# $NetBSD: SimpleMatch.pm,v 1.3 2015/10/11 21:23:34 rillig Exp $
-#
-# A SimpleMatch is the result of applying a regular expression to a Perl
-# scalar value. It can return the range and the text of the captured
-# groups.
-#
-package PkgLint::SimpleMatch;
-
-use strict;
-use warnings;
-
-use enum qw(STRING STARTS ENDS N);
-
-sub new($$) {
-	my ($class, $string, $starts, $ends) = @_;
-	my ($self) = ([$string, [@{$starts}], [@{$ends}], $#{$ends}]);
-	bless($self, $class);
-	return $self;
-}
-
-sub string($)		{ return shift()->[STRING]; }
-sub n($)		{ return shift()->[N]; }
-
-sub has($$) {
-	my ($self, $n) = @_;
-
-	return 0 <= $n && $n <= $self->n
-	    && defined($self->[STARTS]->[$n])
-	    && defined($self->[ENDS]->[$n]);
-}
-
-sub text($$) {
-	my ($self, $n) = @_;
-
-	my $start = $self->[STARTS]->[$n];
-	my $end = $self->[ENDS]->[$n];
-	return substr($self->string, $start, $end - $start);
-}
-
-sub range($$) {
-	my ($self, $n) = @_;
-
-	return ($self->[STARTS]->[$n], $self->[ENDS]->[$n]);
-}
 # $NetBSD: Line.pm,v 1.5 2015/10/11 21:23:34 rillig Exp $
 #
 # When files are read in by pkglint, they are interpreted in terms of
