63bfb60a159c1c5468922cf1b5414789428c4103
Roland Illig <roland.illig@gmx.de>
2015-11-15 09:36:14 +0100

    go fmt

--- shell_test.go
+++ shell_test.go
@@ -28,15 +28,15 @@ func (s *Suite) TestChecklineMkShelltext(c *check.C) {
 func (s *Suite) TestChecklineMkShellword(c *check.C) {
 	s.UseCommandLine("-Wall")
 	line := NewLine("fname", "1", "dummy", nil)
-	
+
 	c.Check(matches("${list}", `^`+reVarname+`$`), equals, true)
 	c.Check(matches("${list}", `^`+reVarnameDirect+`$`), equals, false)
-	
+
 	checklineMkShellword(line, "${${list}}", false)
-	
+
 	c.Check(s.Output(), equals, "")
 
 	checklineMkShellword(line, "\"$@\"", false)
-	
+
 	c.Check(s.Output(), equals, "WARN: fname:1: Please use \"${.TARGET}\" instead of \"$@\".\n")
 }
