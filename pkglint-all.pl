4c23b39137ff206c3769b9adaff201e6784192ec
Roland Illig <roland.illig@gmx.de>
2015-11-15 21:10:03 +0100

    removed CVS_Entry, Change

--- pkglint-all.pl
+++ pkglint-all.pl
@@ -44,49 +44,6 @@ sub save_autofix_changes($) {
 	}
 }
 
-# $NetBSD: CVS_Entry.pm,v 1.3 2015/10/11 21:23:34 rillig Exp $
-#
-# One line from a CVS/Entries file.
-#
-package PkgLint::CVS_Entry;
-
-use strict;
-use warnings;
-
-use enum qw(FNAME REVISION MTIME TAG);
-
-sub new($$$$$) {
-	my ($class, $fname, $revision, $date, $tag) = @_;
-	my $self = [ $fname, $revision, $date, $tag ];
-	bless($self, $class);
-	return $self;
-}
-sub fname($)			{ return shift()->[FNAME]; }
-sub revision($)			{ return shift()->[REVISION]; }
-sub mtime($)			{ return shift()->[MTIME]; }
-sub tag($)			{ return shift()->[TAG]; }
-# $NetBSD: Change.pm,v 1.3 2015/10/11 21:23:34 rillig Exp $
-#
-# A change entry from doc/CHANGES-*
-#
-package PkgLint::Change;
-
-use strict;
-use warnings;
-
-sub new($$$$$$) {
-	my ($class, $line, $action, $pkgpath, $version, $author, $date) = @_;
-	my $self = [ $line, $action, $pkgpath, $version, $author, $date ];
-	bless($self, $class);
-	return $self;
-}
-sub line($)			{ return shift()->[0]; }
-sub action($)			{ return shift()->[1]; }
-sub pkgpath($)			{ return shift()->[2]; }
-sub version($)			{ return shift()->[3]; }
-sub author($)			{ return shift()->[4]; }
-sub date($)			{ return shift()->[5]; }
-
 package pkglint;
 #==========================================================================
 # This package contains the application-specific code of pkglint.
