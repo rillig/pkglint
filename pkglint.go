cf8bb85d63eeac067eca1caed3a56c7367ad66d0
Roland Illig <roland.illig@gmx.de>
2015-11-15 11:29:54 +0100

    Fixed forgotten variable interpolation

--- pkglint.go
+++ pkglint.go
@@ -458,7 +458,7 @@ func checklineRelativePkgdir(line *Line, pkgdir string) {
 
 	if m, otherpkgpath := match1(pkgdir, `^(?:\./)?\.\./\.\./([^/]+/[^/]+)$`); m {
 		if !fileExists(G.globalData.pkgsrcdir + "/" + otherpkgpath + "/Makefile") {
-			line.errorf("There is no package in otherpkgpath.")
+			line.errorf("There is no package in %s.", otherpkgpath)
 		}
 
 	} else {
