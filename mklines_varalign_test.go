fd6af96920b3bd831d9000056f0807ac0be1df6b
Roland Illig <roland.illig@gmx.de>
2018-01-26 20:49:42 +0100

    Ensure that intentionally indented continuation lines are kept

--- mklines_varalign_test.go
+++ mklines_varalign_test.go
@@ -799,3 +799,20 @@ func (s *Suite) Test_Varalign__tabbed_outlier(c *check.C) {
 		".endif")
 	vt.Run()
 }
+
+// When all continuation lines are indented exactly one tab more than the
+// initial line, this is intentional.
+func (s *Suite) Test_Varalign__indented_continuation_line(c *check.C) {
+	vt := NewVaralignTester(s, c)
+	vt.Input(
+		"CONF_FILES_PERMS=\tsource \\",
+		"\t\t\t\tdestination",
+		"\t\t\t\tuser group 0644")
+	vt.Diagnostics()
+	vt.Autofixes()
+	vt.Fixed(
+		"CONF_FILES_PERMS=       source \\",
+		"                                destination",
+		"                                user group 0644")
+	vt.Run()
+}
