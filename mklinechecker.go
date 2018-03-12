0c8494c10d8383dc47c5b8580218b51933ab4d77
Roland Illig <roland.illig@gmx.de>
2018-03-12 21:04:21 +0100

    Fix nil pointer dereference while checking ${VAR:=suffix}

--- mklinechecker.go
+++ mklinechecker.go
@@ -318,7 +318,7 @@ func (ck MkLineChecker) CheckVaruse(varuse *MkVarUse, vuc *VarUseContext) {
 		mkline.Warnf("%s is used but not defined. Spelling mistake?", varname)
 	}
 
-	if hasPrefix(varuse.Mod(), ":=") && !vartype.IsConsideredList() {
+	if hasPrefix(varuse.Mod(), ":=") && vartype != nil && !vartype.IsConsideredList() {
 		mkline.Warnf("The :from=to modifier should only be used with lists.")
 	}
 
