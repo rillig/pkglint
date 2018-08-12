4bec62a76656414a8b4c89c6b7a46a8da5af3c39
Roland Illig <roland.illig@gmx.de>
2018-08-12 21:29:02 +0200

    Test loading variable types from pkgsrc infrastructure files

--- vardefs_test.go
+++ vardefs_test.go
@@ -8,14 +8,31 @@ func (s *Suite) Test_InitVartypes__enumFrom(c *check.C) {
 	t.SetupFileMkLines("editors/emacs/modules.mk",
 		MkRcsID,
 		"",
-		"_EMACS_VERSIONS_ALL=\temacs31",
-		"_EMACS_VERSIONS_ALL=\tignored")
+		"_EMACS_VERSIONS_ALL=    emacs31",
+		"_EMACS_VERSIONS_ALL=    ignored")
+	t.SetupFileMkLines("mk/java-vm.mk",
+		MkRcsID,
+		"",
+		"_PKG_JVMS.8=            openjdk8 oracle-jdk8",
+		"_PKG_JVMS.7=            ${_PKG_JVMS.8} openjdk7 sun-jdk7",
+		"_PKG_JVMS.6=            ${_PKG_JVMS.7} sun-jdk6 jdk16")
+	t.SetupFileMkLines("mk/compiler.mk",
+		MkRcsID,
+		"",
+		".for _version_ in gnu++14 c++14 gnu++11 c++11 gnu++0x c++0x gnu++03 c++03",
+		".  if !empty(USE_LANGUAGES:M${_version_})",
+		"USE_LANGUAGES+=		c++",
+		".  endif",
+		".endfor")
 	mklines := t.SetupFileMkLines("Makefile",
 		MkRcsID,
 		"")
 
-	t.SetupVartypes()
-	vartype := mklines.mklines[1].VariableType("EMACS_VERSIONS_ACCEPTED")
+	vartype := func(varname string) string { return mklines.mklines[1].VariableType(varname).String() }
 
-	c.Check(vartype.String(), equals, "ShellList of enum: emacs31 ")
+	t.SetupVartypes()
+
+	c.Check(vartype("EMACS_VERSIONS_ACCEPTED"), equals, "ShellList of enum: emacs31 ")
+	c.Check(vartype("PKG_JVM"), equals, "enum: jdk16 openjdk7 openjdk8 oracle-jdk8 sun-jdk6 sun-jdk7 ")
+	c.Check(vartype("USE_LANGUAGES"), equals, "ShellList of enum: ada c c++ c++03 c++0x c++11 c++14 c99 fortran fortran77 gnu++03 gnu++0x gnu++11 gnu++14 java obj-c++ objc ")
 }
