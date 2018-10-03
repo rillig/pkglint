ce297e4d24263feb771d114c35a46d4afe9e4eb2
Roland Illig <roland.illig@gmx.de>
2018-10-03 19:49:06 +0200

    Test__test_names: don't mix function definitions and actual code

--- testnames_test.go
+++ testnames_test.go
@@ -14,96 +14,117 @@ import (
 //
 //  Test_${Type}_${Method}__${description_using_underscores}
 func (s *Suite) Test__test_names(c *check.C) {
-	var allowed []string
 
-	handleDecl := func(decl ast.Decl) {
+	// addTestee adds a single type or function declaration
+	// to the testees.
+	addTestee := func(testees *[]string, decl ast.Decl) {
 		switch decl := decl.(type) {
-		case *ast.FuncDecl:
-			typeName := ""
-			if decl.Recv != nil {
-				typeExpr := decl.Recv.List[0].Type.(ast.Expr)
-				if star, ok := typeExpr.(*ast.StarExpr); ok {
-					typeName = star.X.(*ast.Ident).Name + "."
-				} else {
-					typeName = typeExpr.(*ast.Ident).Name + "."
-				}
-			}
-			allowed = append(allowed, strings.Replace(typeName, "Impl.", ".", 1)+decl.Name.Name)
 
 		case *ast.GenDecl:
 			for _, spec := range decl.Specs {
 				switch spec := spec.(type) {
 				case *ast.TypeSpec:
-					allowed = append(allowed, spec.Name.Name)
+					*testees = append(*testees, spec.Name.Name)
 				}
 			}
+
+		case *ast.FuncDecl:
+			typePrefix := ""
+			if decl.Recv != nil {
+				typeExpr := decl.Recv.List[0].Type.(ast.Expr)
+				var typeName string
+				if star, ok := typeExpr.(*ast.StarExpr); ok {
+					typeName = star.X.(*ast.Ident).Name
+				} else {
+					typeName = typeExpr.(*ast.Ident).Name
+				}
+				typePrefix = strings.TrimSuffix(typeName, "Impl") + "."
+			}
+			*testees = append(*testees, typePrefix+decl.Name.Name)
 		}
 	}
 
-	collect := func() {
+	// loadAllTestees returns all type, function and method names
+	// from the current package, in the form FunctionName or
+	// TypeName.MethodName (omitting the * from the type name).
+	loadAllTestees := func() []string {
 		fset := token.NewFileSet()
 		pkgs, err := parser.ParseDir(fset, ".", func(fi os.FileInfo) bool { return true }, 0)
 		if err != nil {
 			panic(err)
 		}
 
+		var typesAndFunctions []string
 		for _, file := range pkgs["main"].Files {
 			for _, decl := range file.Decls {
-				handleDecl(decl)
+				addTestee(&typesAndFunctions, decl)
 			}
 		}
 
-		sort.SliceStable(allowed, func(i, j int) bool { return allowed[i] < allowed[j] })
+		sort.Strings(typesAndFunctions)
+		return typesAndFunctions
 	}
 
-	collect()
+	generateAllowedPrefixes := func(typesAndFunctions []string) map[string]bool {
+		prefixes := make(map[string]bool)
+		for _, funcName := range typesAndFunctions {
+			prefix := strings.Replace(funcName, ".", "_", 1)
+			prefixes[prefix] = true
+		}
 
-	prefixes := make(map[string]bool)
-	for _, funcName := range allowed {
-		prefix := strings.Replace(funcName, ".", "_", 1)
-		prefixes[prefix] = true
+		// Allow some special test name prefixes.
+		prefixes["Varalign"] = true
+		prefixes["ShellParser"] = true
+		return prefixes
 	}
-	prefixes["Varalign"] = true
-	prefixes["ShellParser"] = true
 
-	for _, funcName := range allowed {
-		typeAndMethod := strings.SplitN(funcName, ".", 2)
-		if len(typeAndMethod) == 2 {
-			method := typeAndMethod[1]
-			switch {
-			case !strings.HasPrefix(method, "Test"):
-				// Ignore
-
-			case strings.HasPrefix(method, "Test__"):
-				// OK
-
-			case strings.HasPrefix(method, "Test_"):
-				refAndDescr := strings.SplitN(method[5:], "__", 2)
-				testee := refAndDescr[0]
-				if !prefixes[testee] {
-					c.Errorf("%s: Testee %q not found.\n", funcName, testee)
-				}
-				if len(refAndDescr) == 1 {
-					break
-				}
-				descr := refAndDescr[1]
-				if matches(descr, `\p{Ll}\p{Lu}`) {
-					switch descr {
-					case "comparing_YesNo_variable_to_string",
-						"GitHub",
-						"enumFrom",
-						"dquotBacktDquot",
-						"and_getSubdirs":
-						// These exceptions are ok.
-
-					default:
-						c.Errorf("%s: Test description must not use CamelCase.\n", funcName)
-					}
-				}
+	checkTestName := func(fullTestMethod string, testee string, descr string, prefixes map[string]bool) {
+		if !prefixes[testee] {
+			c.Errorf("%s: Testee %q not found.\n", fullTestMethod, testee)
+		}
+		if matches(descr, `\p{Ll}\p{Lu}`) {
+			switch descr {
+			case "comparing_YesNo_variable_to_string",
+				"GitHub",
+				"enumFrom",
+				"dquotBacktDquot",
+				"and_getSubdirs":
+				// These exceptions are ok.
 
 			default:
-				c.Errorf("%s: Missing underscore.\n", funcName)
+				c.Errorf("%s: Test description must not use CamelCase.\n", fullTestMethod)
 			}
 		}
 	}
+
+	checkAll := func(typesAndFunctions []string, prefixes map[string]bool) {
+		for _, funcName := range typesAndFunctions {
+			typeAndMethod := strings.SplitN(funcName, ".", 2)
+			if len(typeAndMethod) == 2 {
+				method := typeAndMethod[1]
+				switch {
+				case !hasPrefix(method, "Test"):
+					// Ignore
+
+				case hasPrefix(method, "Test__"):
+					// OK
+
+				case hasPrefix(method, "Test_"):
+					refAndDescr := strings.SplitN(method[5:], "__", 2)
+					descr := ""
+					if len(refAndDescr) > 1 {
+						descr = refAndDescr[1]
+					}
+					checkTestName(funcName, refAndDescr[0], descr, prefixes)
+
+				default:
+					c.Errorf("%s: Missing underscore.\n", funcName)
+				}
+			}
+		}
+	}
+
+	testees := loadAllTestees()
+	prefixes := generateAllowedPrefixes(testees)
+	checkAll(testees, prefixes)
 }
