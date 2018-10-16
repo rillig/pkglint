15ee11fe64de1bf21b9b4db46e68fff5efce290e
Roland Illig <roland.illig@gmx.de>
2018-10-16 21:56:23 +0200

    Add lexer methods for matching character classes, or byte sets
    Since pkglint basically only deals with ASCII, and since most grammars
    (like POSIX shell and Makefiles) find all their syntactical characters
    in ASCII too, this part is important to match with good performance.

--- textproc/lexer.go
+++ textproc/lexer.go
@@ -9,7 +9,16 @@ type Lexer struct {
 	rest string
 }
 
+// LexerMark remembers a position in the string being parsed, to be able
+// to revert to that position, should a complex expression not match in
+// its entirety.
 type LexerMark string
+
+// ByteSet is a subset of all 256 possible byte values.
+// It is used for matching character classes efficiently.
+type ByteSet struct {
+	bits [4]uint64
+}
 
 func NewLexer(text string) *Lexer {
 	return &Lexer{text}
@@ -21,16 +30,17 @@ func (l *Lexer) Rest() string { return l.rest }
 // EOF returns whether the whole input has been consumed.
 func (l *Lexer) EOF() bool { return l.rest == "" }
 
-// Mark returns the current position of the lexer,
-// which can later be restored by calling Reset.
-func (l *Lexer) Mark() LexerMark {
-	return LexerMark(l.rest)
+// PeekByte returns the next byte, or -1 at the end.
+func (l *Lexer) PeekByte() int {
+	if l.rest != "" {
+		return int(l.rest[0])
+	}
+	return -1
 }
 
-// Reset sets the lexer back to the position where
-// the corresponding Mark was called.
-func (l *Lexer) Reset(mark LexerMark) {
-	l.rest = string(mark)
+// Skip skips the next n bytes.
+func (l *Lexer) Skip(n int) {
+	l.rest = l.rest[n:]
 }
 
 // NextString tests whether the remaining text has the given prefix,
@@ -50,7 +60,7 @@ func (l *Lexer) NextHspace() string {
 	// The Go 1.11 compiler does not inline constant function arguments.
 	i := 0
 	rest := l.rest
-	for len(rest) > i && (rest[i] == ' ' || rest[i] == '\t') {
+	for i < len(rest) && (rest[i] == ' ' || rest[i] == '\t') {
 		i++
 	}
 	if i != 0 {
@@ -77,7 +87,7 @@ func (l *Lexer) NextByte(b byte) bool {
 func (l *Lexer) NextBytesFunc(fn func(b byte) bool) string {
 	i := 0
 	rest := l.rest
-	for len(rest) > i && fn(rest[i]) {
+	for i < len(rest) && fn(rest[i]) {
 		i++
 	}
 	if i != 0 {
@@ -85,3 +95,85 @@ func (l *Lexer) NextBytesFunc(fn func(b byte) bool) string {
 	}
 	return rest[:i]
 }
+
+// NextByteSet chops off and returns the first byte if the set contains it, otherwise -1.
+func (l *Lexer) NextByteSet(bytes *ByteSet) int {
+	rest := l.rest
+	if 0 < len(rest) && bytes.bits[rest[0]/64]&(1<<(rest[0]%64)) != 0 {
+		l.rest = rest[1:]
+		return int(rest[0])
+	}
+	return -1
+}
+
+// NextBytesSet chops off the longest prefix consisting solely of bytes
+// from the given set.
+func (l *Lexer) NextBytesSet(bytes *ByteSet) string {
+	i := 0
+	rest := l.rest
+	for i < len(rest) && bytes.bits[rest[i]/64]&(1<<(rest[i]%64)) != 0 {
+		i++
+	}
+	if i != 0 {
+		l.rest = rest[i:]
+	}
+	return rest[:i]
+}
+
+// Mark returns the current position of the lexer,
+// which can later be restored by calling Reset.
+func (l *Lexer) Mark() LexerMark {
+	return LexerMark(l.rest)
+}
+
+// Reset sets the lexer back to the position where
+// the corresponding Mark was called.
+func (l *Lexer) Reset(mark LexerMark) {
+	l.rest = string(mark)
+}
+
+// Since returns the text between the given mark and the current position.
+func (l *Lexer) Since(mark LexerMark) string {
+	return string(mark)[0 : len(mark)-len(l.rest)]
+}
+
+// NewByteSet creates a bit mask out of a string like "0-9A-Za-z_".
+// The bit mask can be used with Lexer.NextBytesSet.
+func NewByteSet(chars string) *ByteSet {
+	var set ByteSet
+	i := 0
+
+	for i < len(chars) {
+		switch {
+		case i+2 < len(chars) && chars[i+1] == '-':
+			for ch := uint16(chars[i]); ch <= uint16(chars[i+2]); ch++ {
+				set.bits[ch/64] |= 1 << (ch % 64)
+			}
+			i += 3
+		default:
+			ch := chars[i]
+			set.bits[ch/64] |= 1 << (ch % 64)
+			i++
+		}
+	}
+	return &set
+}
+
+// Inverse returns a byte set that matches the inverted set of bytes.
+func (bs *ByteSet) Inverse() *ByteSet {
+	return &ByteSet{
+		[4]uint64{
+			^bs.bits[0],
+			^bs.bits[1],
+			^bs.bits[2],
+			^bs.bits[3]}}
+}
+
+// Predefined byte classes for parsing ASCII text.
+var (
+	Alnum  = NewByteSet("A-Za-z0-9")  // Alphanumerical, without underscore
+	AlnumU = NewByteSet("A-Za-z0-9_") // Alphanumerical, including underscore
+	Digit  = NewByteSet("0-9")        // The digits zero to nine
+	Space  = NewByteSet("\t\n ")      // Tab, newline, space
+	Hspace = NewByteSet("\t ")        // Tab, space
+)
--- textproc/lexer_test.go
+++ textproc/lexer_test.go
@@ -11,14 +11,20 @@ type Suite struct{}
 var equals = check.Equals
 
 func Test(t *testing.T) {
-	check.Suite(Suite{})
+	check.Suite(new(Suite))
 	check.TestingT(t)
 }
 
 func (s *Suite) Test_NewLexer(c *check.C) {
 	lexer := NewLexer("text")
 
-	c.Check(lexer.EOF(), equals, false)
+	c.Check(lexer.rest, equals, "text")
+}
+
+func (s *Suite) Test_Lexer_Rest__initial(c *check.C) {
+	lexer := NewLexer("text")
+
+	c.Check(lexer.Rest(), equals, "text")
 }
 
 func (s *Suite) Test_Lexer_Rest__end(c *check.C) {
@@ -27,10 +33,10 @@ func (s *Suite) Test_Lexer_Rest__end(c *check.C) {
 	c.Check(lexer.Rest(), equals, "")
 }
 
-func (s *Suite) Test_Lexer_Rest__middle(c *check.C) {
+func (s *Suite) Test_Lexer_EOF__initial(c *check.C) {
 	lexer := NewLexer("text")
 
-	c.Check(lexer.Rest(), equals, "text")
+	c.Check(lexer.EOF(), equals, false)
 }
 
 func (s *Suite) Test_Lexer_EOF__end(c *check.C) {
@@ -39,17 +45,95 @@ func (s *Suite) Test_Lexer_EOF__end(c *check.C) {
 	c.Check(lexer.EOF(), equals, true)
 }
 
-func (s *Suite) Test_Lexer_EOF__middle(c *check.C) {
+func (s *Suite) Test_Lexer_PeekByte(c *check.C) {
 	lexer := NewLexer("text")
 
-	c.Check(lexer.EOF(), equals, false)
+	c.Check(lexer.PeekByte(), equals, int('t'))
+
+	c.Check(lexer.NextString("text"), equals, "text")
+
+	c.Check(lexer.PeekByte(), equals, -1)
 }
 
-func (s *Suite) Test_Lexer_EOF__reached(c *check.C) {
-	lexer := NewLexer("text")
-	lexer.NextString("text")
+func (s *Suite) Test_Lexer_Skip(c *check.C) {
+	lexer := NewLexer("example text")
 
-	c.Check(lexer.EOF(), equals, true)
+	lexer.Skip(7)
+
+	c.Check(lexer.Rest(), equals, " text")
+
+	// Skipping a fixed number of bytes only makes sense when the
+	// lexer has examined every one of them before. Therefore no
+	// extra check is done here, and panicking here is intentional.
+	c.Check(
+		func() { lexer.Skip(6) },
+		check.PanicMatches,
+		`^runtime error: slice bounds out of range$`)
+}
+
+func (s *Suite) Test_Lexer_NextString(c *check.C) {
+	lexer := NewLexer("text")
+
+	c.Check(lexer.NextString("te"), equals, "te")
+	c.Check(lexer.NextString("st"), equals, "") // Did not match.
+	c.Check(lexer.NextString("xt"), equals, "xt")
+}
+
+func (s *Suite) Test_Lexer_NextHspace(c *check.C) {
+	lexer := NewLexer("spaces   \t \t  and tabs\n")
+
+	c.Check(lexer.NextString("spaces"), equals, "spaces")
+	c.Check(lexer.NextHspace(), equals, "   \t \t  ")
+	c.Check(lexer.NextHspace(), equals, "") // No space left.
+	c.Check(lexer.NextString("and tabs"), equals, "and tabs")
+	c.Check(lexer.NextHspace(), equals, "") // Newline is not a horizontal space.
+}
+
+func (s *Suite) Test_Lexer_NextByte(c *check.C) {
+	lexer := NewLexer("byte")
+
+	c.Check(lexer.NextByte('b'), equals, true)
+	c.Check(lexer.NextByte('b'), equals, false) // The b is already chopped off.
+	c.Check(lexer.NextByte('y'), equals, true)
+	c.Check(lexer.NextByte('t'), equals, true)
+	c.Check(lexer.NextByte('e'), equals, true)
+	c.Check(lexer.NextByte(0), equals, false) // This is not a C string.
+}
+
+func (s *Suite) Test_Lexer_NextBytesFunc(c *check.C) {
+	lexer := NewLexer("an alphanumerical string")
+
+	c.Check(lexer.NextBytesFunc(func(b byte) bool { return 'A' <= b && b <= 'Z' }), equals, "")
+	c.Check(lexer.NextBytesFunc(func(b byte) bool { return !unicode.IsSpace(rune(b)) }), equals, "an")
+	c.Check(lexer.NextHspace(), equals, " ")
+	c.Check(lexer.NextBytesFunc(func(b byte) bool { return 'a' <= b && b <= 'z' }), equals, "alphanumerical")
+	c.Check(lexer.NextBytesFunc(func(b byte) bool { return true }), equals, " string")
+}
+
+func (s *Suite) Test_Lexer_NextByteSet(c *check.C) {
+	lexer := NewLexer("an a\n")
+
+	c.Check(lexer.NextByteSet(Alnum), equals, int('a'))
+	c.Check(lexer.NextByteSet(Alnum), equals, int('n'))
+	c.Check(lexer.NextByteSet(Space), equals, int(' '))
+	c.Check(lexer.NextByteSet(Alnum), equals, int('a'))
+	c.Check(lexer.NextByteSet(Space), equals, int('\n'))
+	c.Check(lexer.NextByteSet(Alnum), equals, -1)
+}
+
+func (s *Suite) Test_Lexer_NextBytesSet(c *check.C) {
+	lexer := NewLexer("an alphanumerical 90_ \tstring\t\t \n")
+
+	c.Check(lexer.NextBytesSet(Alnum), equals, "an")
+	c.Check(lexer.NextBytesSet(Alnum), equals, "")
+	c.Check(lexer.NextBytesSet(Space), equals, " ")
+	c.Check(lexer.NextBytesSet(Alnum), equals, "alphanumerical")
+	c.Check(lexer.NextBytesSet(Space), equals, " ")
+	c.Check(lexer.NextBytesSet(AlnumU), equals, "90_")
+	c.Check(lexer.NextBytesSet(Space), equals, " \t")
+	c.Check(lexer.NextBytesSet(Alnum), equals, "string")
+	c.Check(lexer.NextBytesSet(Hspace), equals, "\t\t ")
+	c.Check(lexer.NextBytesSet(Space), equals, "\n")
 }
 
 func (s *Suite) Test_Lexer_Mark__beginning(c *check.C) {
@@ -111,41 +195,43 @@ func (s *Suite) Test_Lexer_Reset__multiple(c *check.C) {
 	c.Check(lexer.Rest(), equals, "")
 }
 
-func (s *Suite) Test_Lexer_NextString(c *check.C) {
+func (s *Suite) Test_Lexer__NextString_then_EOF(c *check.C) {
 	lexer := NewLexer("text")
+	lexer.NextString("text")
+
+	c.Check(lexer.EOF(), equals, true)
+}
+
+func (s *Suite) Test_Lexer_Since(c *check.C) {
+	lexer := NewLexer("text")
+	mark := lexer.Mark()
 
 	c.Check(lexer.NextString("te"), equals, "te")
 	c.Check(lexer.NextString("st"), equals, "") // Did not match.
+	c.Check(lexer.Since(mark), equals, "te")
 	c.Check(lexer.NextString("xt"), equals, "xt")
+	c.Check(lexer.Since(mark), equals, "text")
 }
 
-func (s *Suite) Test_Lexer_NextHspace(c *check.C) {
-	lexer := NewLexer("spaces   \t \t  and tabs\n")
+func (s *Suite) Test_NewByteSet(c *check.C) {
+	set := NewByteSet("A-Za-z0-9_\xFC")
 
-	c.Check(lexer.NextString("spaces"), equals, "spaces")
-	c.Check(lexer.NextHspace(), equals, "   \t \t  ")
-	c.Check(lexer.NextHspace(), equals, "") // No space left.
-	c.Check(lexer.NextString("and tabs"), equals, "and tabs")
-	c.Check(lexer.NextHspace(), equals, "") // Newline is not a horizontal space.
+	c.Check(set.bits, equals, [4]uint64{
+		0x03ff000000000000, // 9-0
+		0x07fffffe87fffffe, // z-a _ Z-A
+		0x0000000000000000,
+		0x1000000000000000}) // ü
 }
 
-func (s *Suite) Test_Lexer_NextByte(c *check.C) {
-	lexer := NewLexer("byte")
+func (s *Suite) Test_ByteSet_Inverse(c *check.C) {
+	set := NewByteSet("A-Za-z0-9_\xFC")
+	inverse := set.Inverse()
 
-	c.Check(lexer.NextByte('b'), equals, true)
-	c.Check(lexer.NextByte('b'), equals, false) // The b is already chopped off.
-	c.Check(lexer.NextByte('y'), equals, true)
-	c.Check(lexer.NextByte('t'), equals, true)
-	c.Check(lexer.NextByte('e'), equals, true)
-	c.Check(lexer.NextByte(0), equals, false) // This is not a C string.
-}
+	c.Check(inverse.bits, equals, [4]uint64{
+		0xfc00ffffffffffff,
+		0xf800000178000001,
+		0xffffffffffffffff,
+		0xefffffffffffffff})
 
-func (s *Suite) Test_Lexer_NextBytesFunc(c *check.C) {
-	lexer := NewLexer("an alphanumerical string")
-
-	c.Check(lexer.NextBytesFunc(func(b byte) bool { return 'A' <= b && b <= 'Z' }), equals, "")
-	c.Check(lexer.NextBytesFunc(func(b byte) bool { return !unicode.IsSpace(rune(b)) }), equals, "an")
-	c.Check(lexer.NextHspace(), equals, " ")
-	c.Check(lexer.NextBytesFunc(func(b byte) bool { return 'a' <= b && b <= 'z' }), equals, "alphanumerical")
-	c.Check(lexer.NextBytesFunc(func(b byte) bool { return true }), equals, " string")
+	c.Check(inverse.Inverse().bits, equals, set.bits)
 }
