d42ff3cbdabf90f63a6684374fa06b2f16add692
Roland Illig <roland.illig@gmx.de>
2018-10-25 20:59:16 +0200

    Try to find a real-life example for exotic case in VariableNeedsQuoting

--- mklinechecker.go
+++ mklinechecker.go
@@ -489,6 +489,10 @@ func (ck MkLineChecker) checkVarusePermissions(varname string, vartype *Vartype,
 		needed := aclpUse
 		if directly || indirectly {
 			needed = aclpUseLoadtime
+			if trace.Tracing {
+				trace.Stepf("Uncovered code")
+			}
+			panic("Uncovered code")
 		}
 		alternativeFiles := vartype.AllowedFiles(needed)
 		if alternativeFiles != "" {
