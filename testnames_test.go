a11f2fd3380c0e949151c92da7b0e955004a1bfb
Roland Illig <roland.illig@gmx.de>
2018-10-26 22:58:42 +0200

    In Test__test_names, collect errors and warnings before printing them

--- testnames_test.go
+++ testnames_test.go
@@ -22,25 +22,34 @@ func (s *Suite) Test__test_names(c *check.C) {
 		File string
 	}
 
-	elementString := func(e Element) string {
+	var errors []string
+	var warnings []string
+	addError := func(format string, args ...interface{}) {
+		errors = append(errors, "E: "+sprintf(format, args...))
+	}
+	addWarning := func(format string, args ...interface{}) {
+		warnings = append(warnings, "W: "+sprintf(format, args...))
+	}
+
+	elementString := func(e *Element) string {
 		sep := ifelseStr(e.Type != "" && e.Func != "", ".", "")
 		return fmt.Sprintf("%s: %s%s%s", e.File, e.Type, sep, e.Func)
 	}
 
-	isTest := func(e Element) bool {
+	isTest := func(e *Element) bool {
 		return hasSuffix(e.File, "_test.go") && e.Type != "" && hasPrefix(e.Func, "Test")
 	}
 
 	// addElement adds a single type or function declaration
 	// to the known elements.
-	addElement := func(elements *[]Element, decl ast.Decl, fileName string) {
+	addElement := func(elements *[]*Element, decl ast.Decl, fileName string) {
 		switch decl := decl.(type) {
 
 		case *ast.GenDecl:
 			for _, spec := range decl.Specs {
 				switch spec := spec.(type) {
 				case *ast.TypeSpec:
-					*elements = append(*elements, Element{spec.Name.Name, "", fileName})
+					*elements = append(*elements, &Element{spec.Name.Name, "", fileName})
 				}
 			}
 
@@ -55,21 +64,21 @@ func (s *Suite) Test__test_names(c *check.C) {
 				}
 				typeName = strings.TrimSuffix(typeName, "Impl")
 			}
-			*elements = append(*elements, Element{typeName, decl.Name.Name, fileName})
+			*elements = append(*elements, &Element{typeName, decl.Name.Name, fileName})
 		}
 	}
 
 	// loadAllElements returns all type, function and method names
 	// from the current package, in the form FunctionName or
 	// TypeName.MethodName (omitting the * from the type name).
-	loadAllElements := func() []Element {
+	loadAllElements := func() []*Element {
 		fileSet := token.NewFileSet()
 		pkgs, err := parser.ParseDir(fileSet, ".", func(fi os.FileInfo) bool { return true }, 0)
 		if err != nil {
 			panic(err)
 		}
 
-		var elements []Element
+		var elements []*Element
 		for fileName, file := range pkgs["main"].Files {
 			for _, decl := range file.Decls {
 				addElement(&elements, decl, fileName)
@@ -92,7 +101,7 @@ func (s *Suite) Test__test_names(c *check.C) {
 		return elements
 	}
 
-	testName := func(element Element) string {
+	testName := func(element *Element) string {
 		if isTest(element) {
 			return ""
 		}
@@ -100,29 +109,29 @@ func (s *Suite) Test__test_names(c *check.C) {
 		return element.Type + sep + element.Func
 	}
 
-	// generateTesteeNames generates a map containing the names of all
+	// collectTesteeByName generates a map containing the names of all
 	// testable elements, as used in the test names. Examples:
 	//
 	//  Autofix
 	//  Line_Warnf
 	//  match5
-	generateTesteeNames := func(elements []Element) map[string]bool {
-		prefixes := make(map[string]bool)
+	collectTesteeByName := func(elements []*Element) map[string]*Element {
+		prefixes := make(map[string]*Element)
 		for _, element := range elements {
 			if prefix := testName(element); prefix != "" {
-				prefixes[prefix] = true
+				prefixes[prefix] = element
 			}
 		}
 
-		// Allow some special test name prefixes.
-		prefixes["Varalign"] = true
-		prefixes["ShellParser"] = true
+		// Allow some special test name testeeByName.
+		prefixes["Varalign"] = &Element{"Varalign", "", "mklines_varalign.go"}
+		prefixes["ShellParser"] = &Element{"ShellParser", "", "mkshparser.go"}
 		return prefixes
 	}
 
-	checkTestName := func(test Element, testee string, descr string, prefixes map[string]bool) {
-		if !prefixes[testee] {
-			c.Errorf("%s: Testee %q not found.\n", elementString(test), testee)
+	checkTestName := func(test *Element, testee string, descr string, testeeByName map[string]*Element) {
+		if testeeByName[testee] == nil {
+			addError("%s: Testee %q not found.", elementString(test), testee)
 		}
 		if matches(descr, `\p{Ll}\p{Lu}`) {
 			switch descr {
@@ -135,12 +144,12 @@ func (s *Suite) Test__test_names(c *check.C) {
 				// These exceptions are ok.
 
 			default:
-				c.Errorf("%s: Test description must not use CamelCase.\n", elementString(test))
+				addError("%s: Test description must not use CamelCase.", elementString(test))
 			}
 		}
 	}
 
-	checkAll := func(elements []Element, prefixes map[string]bool) {
+	checkAll := func(elements []*Element, testeeByName map[string]*Element) {
 		testNames := make(map[string]bool)
 
 		for _, test := range elements {
@@ -157,25 +166,39 @@ func (s *Suite) Test__test_names(c *check.C) {
 						descr = refAndDescr[1]
 					}
 					testNames[refAndDescr[0]] = true
-					checkTestName(test, refAndDescr[0], descr, prefixes)
+					checkTestName(test, refAndDescr[0], descr, testeeByName)
 
 				default:
-					c.Errorf("%s: Missing underscore.\n", elementString(test))
+					addError("%s: Missing underscore.", elementString(test))
 				}
 			}
 		}
 
-		for _, testee := range elements {
-			if !isTest(testee) && !hasSuffix(testee.File, "_test.go") && !hasSuffix(testee.File, "yacc.go") {
-				testNamePrefix := testName(testee)
-				if false && !testNames[testNamePrefix] {
-					fmt.Printf("%s: Does not have a unit test (Test_%s).\n", elementString(testee), testNamePrefix)
+		for _, element := range elements {
+			if !hasSuffix(element.File, "_test.go") && !hasSuffix(element.File, "yacc.go") {
+				testNamePrefix := testName(element)
+				if !testNames[testNamePrefix] {
+					addWarning("%s: Does not have a unit test (Test_%s).", elementString(element), testNamePrefix)
 				}
 			}
 		}
 	}
 
 	elements := loadAllElements()
-	prefixes := generateTesteeNames(elements)
-	checkAll(elements, prefixes)
+	testeeByName := collectTesteeByName(elements)
+	checkAll(elements, testeeByName)
+
+	printWarnings := "no"[0] == 'y' // Set this to "yes" to enable warnings.
+
+	for _, err := range errors {
+		fmt.Println(err)
+	}
+	for _, warning := range warnings {
+		if printWarnings {
+			fmt.Println(warning)
+		}
+	}
+	if len(errors) > 0 || (printWarnings && len(warnings) > 0) {
+		fmt.Printf("%d errors and %d warnings.", len(errors), len(warnings))
+	}
 }
