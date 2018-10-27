1ff544e4f29429beae35390ffb0f7940f49a4b6a
Roland Illig <roland.illig@gmx.de>
2018-10-27 17:32:16 +0200

    Use clearer toggle for showing missing unit tests

--- testnames_test.go
+++ testnames_test.go
@@ -230,7 +230,7 @@ func (s *Suite) Test__test_names(c *check.C) {
 	testeeByName := collectTesteeByName(elements)
 	checkAll(elements, testeeByName)
 
-	printWarnings := "no"[0] == 'y' // Set this to "yes" to enable warnings.
+	printWarnings := func() bool { return true }()
 
 	for _, err := range errors {
 		fmt.Println(err)
