9e504955e27c142a51cb9ed94f9525ed623750ca
Roland Illig <roland.illig@gmx.de>
2018-11-04 21:19:18 +0100

    Maybe add linter for checking references to Go code in comments

--- testnames_test.go
+++ testnames_test.go
@@ -246,3 +246,8 @@ func (s *Suite) Test__test_names(c *check.C) {
 		fmt.Printf("%d errors and %d warnings.", len(errors), len(warnings))
 	}
 }
+
+// XXX: It might be nice to check all comments of the form "See XYZ"
+// to see whether XYZ actually exists. The scope should be the current type,
+// then the current package, then a package-qualified identifier.
+// As if there were a "_ = XYZ" at the beginning of the function.
