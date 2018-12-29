bdbaadcb4d8dd04123ff1ed6468d1c57cd356bbd
Roland Illig <roland.illig@gmx.de>
2018-12-30 00:25:39 +0100

    Change VartypeCheck.MkLine to *MkLineImpl

--- vartypecheck.go
+++ vartypecheck.go
@@ -11,8 +11,10 @@ type VartypeCheck struct {
 	// See https://github.com/golang/go/issues/28972.
 	// That doesn't help though since pkglint contains these "more convoluted alias declarations"
 	// mentioned in https://github.com/golang/go/commit/6971090515ba.
+	// Therefore MkLine is declared as *MkLineImpl here.
+	// Ideally the "more convoluted cyclic type declaration" should be broken up.
 
-	MkLine MkLine
+	MkLine *MkLineImpl
 	Line   Line
 
 	// The name of the variable being checked.
