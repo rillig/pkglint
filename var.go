b788e9ceba55a412eb5f199df6daedf5db874d86
Roland Illig <roland.illig@gmx.de>
2019-02-22 11:22:21 +0100

    Add initial design sketch for variable value analysis

--- var.go
+++ var.go
@@ -2,23 +2,93 @@ package pkglint
 
 // Var describes a variable in a Makefile snippet.
 //
+// It provides information about the possible values and all places where the
+// variable is accessed.
+//
 // TODO: Remove this type in June 2019 if it is still a stub.
 type Var struct {
-	Name string
-	Type *Vartype
+	Name         string
+	Type         *Vartype
+	literalValue *string
 }
 
-func NewVar(name string) *Var { return &Var{name, nil} }
+func NewVar(name string) *Var { return &Var{name, nil, nil} }
 
-// Constant returns whether the variable is only ever assigned a single value,
+// Conditional returns whether the variable value depends on other variables.
+func (v *Var) Conditional() bool {
+	G.Assertf(false, "Not implemented.")
+	return false
+}
+
+// ConditionalVars returns the variables on which the value of this variable depends.
+// Typical cases are:
+//
+// * references in the value (VAR=${OTHER}),
+//
+// * conditions (.if ${OPSYS} == NetBSD), and
+//
+// * loops (.for dir in ${DIRS}; VAR+=${dir}; .endfor).
+func (v *Var) ConditionalVars() []*Var {
+	G.Assertf(false, "Not implemented.")
+	return nil
+}
+
+// Literal returns whether the variable is only ever assigned a single value,
 // without being dependent on any other variable.
 //
-// Multiple assignments (such as VAR=1, VAR+=2, VAR+=3) are considered constant
+// Multiple assignments (such as VAR=1, VAR+=2, VAR+=3) are considered literals
 // as well, as long as the variable is not used in-between these assignments.
-// That is, no .include or .if may appear there, and none of the ::= modifiers may
+// That is, no .include or .if may appear there, and no ::= modifier may
 // be involved.
 //
-// Simple .for loops that append to the variable are ok though.
-func (v *Var) Constant() bool { return false }
+// Simple .for loops that append to the variable are ok as well.
+func (v *Var) Literal() bool {
+	return v.literalValue != nil
+}
 
-func (v *Var) ConstantValue() string { return "" }
+// LiteralValue returns the value of the literal.
+// It is only allowed when Literal() returns true.
+func (v *Var) LiteralValue() string {
+	G.Assertf(v.Literal(), "Variable must have a literal value.")
+	return *v.literalValue
+}
+
+// Value returns the (approximated) value of the variable, taking into account
+// all variable assignments that happen outside the pkgsrc infrastructure.
+//
+// For variables that are conditionally assigned (as in .if/.else), the returned
+// value is not reliable. It may be the value from either branch, or even the
+// combined value of both branches.
+//
+// See Literal and LiteralValue for more reliable information.
+func (v *Var) Value() string {
+	G.Assertf(false, "Not implemented.")
+	return ""
+}
+
+// ValueInfra returns the (approximated) value of the variable, taking into
+// account all variable assignments from the package, the user and the pkgsrc
+// infrastructure.
+//
+// For variables that are conditionally assigned (as in .if/.else), the returned
+// value is not reliable. It may be the value from either branch, or even the
+// combined value of both branches.
+//
+// See Literal and LiteralValue for more reliable information, but these ignore
+// assignments from the infrastructure.
+func (v *Var) ValueInfra() string {
+	G.Assertf(false, "Not implemented.")
+	return ""
+}
+
+// Uses returns the locations where the variable is used.
+func (v *Var) Uses() []Location {
+	G.Assertf(false, "Not implemented.")
+	return nil
+}
+
+// Defs returns the locations where the variable is defined.
+func (v *Var) Defs() []Location {
+	G.Assertf(false, "Not implemented.")
+	return nil
+}
--- var_test.go
+++ var_test.go
@@ -2,18 +2,18 @@ package pkglint
 
 import "gopkg.in/check.v1"
 
-func (s *Suite) Test_Var_Constant(c *check.C) {
+func (s *Suite) Test_Var_Literal(c *check.C) {
 	v := NewVar("VARNAME")
 
 	// FIXME: Replace this test with an actual use case.
 
-	c.Check(v.Constant(), equals, false)
+	c.Check(v.Literal(), equals, false)
 }
 
-func (s *Suite) Test_Var_ConstantValue(c *check.C) {
+func (s *Suite) ignored_Test_Var_LiteralValue(c *check.C) {
 	v := NewVar("VARNAME")
 
 	// FIXME: Replace this test with an actual use case.
 
-	c.Check(v.ConstantValue(), equals, "")
+	c.Check(v.LiteralValue(), equals, "")
 }
