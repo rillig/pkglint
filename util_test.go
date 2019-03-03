2034384959163c1574ccad3f7289a2ab30ef1835
Roland Illig <roland.illig@gmx.de>
2019-03-03 11:25:23 +0100

    Demonstrate a bug in cleanpath
    And I thought that only relpath would be buggy.

--- util_test.go
+++ util_test.go
@@ -114,6 +114,9 @@ func (s *Suite) Test_cleanpath(c *check.C) {
 	test("aa/bb/cc/dd/../..", "aa/bb")
 	test("aa/bb/../../cc/dd/../../ee/ff/buildlink3.mk", "aa/bb/../../ee/ff/buildlink3.mk")
 	test("./aa/bb/../../cc/dd/../../ee/ff/buildlink3.mk", "aa/bb/../../ee/ff/buildlink3.mk")
+
+	// FIXME: Argh, this must be a simple "..".
+	test("../.", "../.")
 }
 
 // Relpath is called so often that handling the most common calls
