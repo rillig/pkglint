72d04f09b9536cf5db306f53097c826c85032c52
Roland Illig <roland.illig@gmx.de>
2019-03-10 20:41:28 +0100

    Inline RedundantScope.Handle

--- redundantscope.go
+++ redundantscope.go
@@ -29,19 +29,15 @@ func NewRedundantScope() *RedundantScope {
 
 func (s *RedundantScope) Check(mklines MkLines) {
 	mklines.ForEach(func(mkline MkLine) {
-		s.Handle(mkline, mklines.indentation)
+		s.updateIncludePath(mkline)
+
+		switch {
+		case mkline.IsVarassign():
+			s.handleVarassign(mkline, mklines.indentation)
+		}
+
+		s.handleVarUse(mkline)
 	})
-}
-
-func (s *RedundantScope) Handle(mkline MkLine, ind *Indentation) {
-	s.updateIncludePath(mkline)
-
-	switch {
-	case mkline.IsVarassign():
-		s.handleVarassign(mkline, ind)
-	}
-
-	s.handleVarUse(mkline)
 }
 
 func (s *RedundantScope) updateIncludePath(mkline MkLine) {
