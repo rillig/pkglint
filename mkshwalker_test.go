055bac98131bc4859993055df7b8f9292b0f6bd4
Roland Illig <roland.illig@gmx.de>
2019-04-08 18:07:52 +0200

    Separate code from data in Test_MkShWalker_Walk

--- mkshwalker_test.go
+++ mkshwalker_test.go
@@ -3,17 +3,15 @@ package pkglint
 import "gopkg.in/check.v1"
 
 func (s *Suite) Test_MkShWalker_Walk(c *check.C) {
-	list, err := parseShellProgram(dummyLine, ""+
-		"if condition; then action; else case selector in pattern) case-item-action ;; esac; fi; "+
-		"set -e; "+
-		"cd ${WRKSRC}/locale; "+
-		"for lang in *.po; do "+
-		"  [ \"$${lang}\" = \"wxstd.po\" ] && continue; "+
-		"  ${TOOLS_PATH.msgfmt} -c -o \"$${lang%.po}.mo\" \"$${lang}\"; "+
-		"done; "+
-		"while :; do fun() { :; } 1>&2; done")
 
-	if c.Check(err, check.IsNil) && c.Check(list, check.NotNil) {
+	test := func(program string, output ...string) {
+
+		list, err := parseShellProgram(dummyLine, program)
+
+		if !c.Check(err, check.IsNil) || !c.Check(list, check.NotNil) {
+			return
+		}
+
 		var commands []string
 		add := func(kind string, format string, args ...interface{}) {
 			if format != "" && !contains(format, "%") {
@@ -58,131 +56,144 @@ func (s *Suite) Test_MkShWalker_Walk(c *check.C) {
 		//    Case with 1 item(s)
 		//      ...
 
-		c.Check(commands, deepEquals, []string{
-			"            List with 5 andOrs",
-			"           AndOr with 1 pipelines",
-			"        Pipeline with 1 commands",
-			"         Command ",
-			" CompoundCommand ",
-			"              If with 1 then-branches",
-			"            List with 1 andOrs",
-			"           AndOr with 1 pipelines",
-			"        Pipeline with 1 commands",
-			"         Command ",
-			"   SimpleCommand condition",
-			"            Path List.AndOr[0].Pipeline[0].Command[0].CompoundCommand.If.List[0].AndOr[0].Pipeline[0].Command[0].SimpleCommand",
-			"            Word condition",
-			"            List with 1 andOrs",
-			"           AndOr with 1 pipelines",
-			"        Pipeline with 1 commands",
-			"         Command ",
-			"   SimpleCommand action",
-			"            Path List.AndOr[0].Pipeline[0].Command[0].CompoundCommand.If.List[1].AndOr[0].Pipeline[0].Command[0].SimpleCommand",
-			"            Word action",
-			"            List with 1 andOrs",
-			"           AndOr with 1 pipelines",
-			"        Pipeline with 1 commands",
-			"         Command ",
-			" CompoundCommand ",
-			"            Case with 1 items",
-			"            Word selector",
-			"        CaseItem with 1 patterns",
-			"           Words with 1 words",
-			"            Word pattern",
-			"            List with 1 andOrs",
-			"           AndOr with 1 pipelines",
-			"        Pipeline with 1 commands",
-			"         Command ",
-			"   SimpleCommand case-item-action",
-			"            Path List.AndOr[0].Pipeline[0].Command[0].CompoundCommand.If." +
-				"List[2].AndOr[0].Pipeline[0].Command[0].CompoundCommand.Case.CaseItem[0]." +
-				"List.AndOr[0].Pipeline[0].Command[0].SimpleCommand",
-			"            Word case-item-action",
-			"           AndOr with 1 pipelines",
-			"        Pipeline with 1 commands",
-			"         Command ",
-			"   SimpleCommand set -e",
-			"            Path List.AndOr[1].Pipeline[0].Command[0].SimpleCommand",
-			"            Word set",
-			"           Words with 1 words",
-			"            Word -e",
-			"           AndOr with 1 pipelines",
-			"        Pipeline with 1 commands",
-			"         Command ",
-			"   SimpleCommand cd ${WRKSRC}/locale",
-			"            Path List.AndOr[2].Pipeline[0].Command[0].SimpleCommand",
-			"            Word cd",
-			"           Words with 1 words",
-			"            Word ${WRKSRC}/locale",
-			"           AndOr with 1 pipelines",
-			"        Pipeline with 1 commands",
-			"         Command ",
-			" CompoundCommand ",
-			"             For variable lang",
-			"         Varname lang",
-			"           Words with 1 words",
-			"            Word *.po",
-			"            List with 2 andOrs",
-			"           AndOr with 2 pipelines",
-			"        Pipeline with 1 commands",
-			"         Command ",
-			"   SimpleCommand [ \"$${lang}\" = \"wxstd.po\" ]",
-			"            Path List.AndOr[3].Pipeline[0].Command[0].CompoundCommand.For.List.AndOr[0].Pipeline[0].Command[0].SimpleCommand",
-			"            Word [",
-			"           Words with 4 words",
-			"            Word \"$${lang}\"",
-			"            Word =",
-			"            Word \"wxstd.po\"",
-			"            Word ]",
-			"        Pipeline with 1 commands",
-			"         Command ",
-			"   SimpleCommand continue",
-			"            Path List.AndOr[3].Pipeline[0].Command[0].CompoundCommand.For.List.AndOr[0].Pipeline[1].Command[0].SimpleCommand",
-			"            Word continue",
-			"           AndOr with 1 pipelines",
-			"        Pipeline with 1 commands",
-			"         Command ",
-			"   SimpleCommand ${TOOLS_PATH.msgfmt} -c -o \"$${lang%.po}.mo\" \"$${lang}\"",
-			"            Path List.AndOr[3].Pipeline[0].Command[0].CompoundCommand.For.List.AndOr[1].Pipeline[0].Command[0].SimpleCommand",
-			"            Word ${TOOLS_PATH.msgfmt}",
-			"           Words with 4 words",
-			"            Word -c",
-			"            Word -o",
-			"            Word \"$${lang%.po}.mo\"",
-			"            Word \"$${lang}\"",
-			"           AndOr with 1 pipelines",
-			"        Pipeline with 1 commands",
-			"         Command ",
-			" CompoundCommand ",
-			"            Loop ",
-			"            List with 1 andOrs",
-			"           AndOr with 1 pipelines",
-			"        Pipeline with 1 commands",
-			"         Command ",
-			"   SimpleCommand :",
-			"            Path List.AndOr[4].Pipeline[0].Command[0].CompoundCommand.Loop.List[0].AndOr[0].Pipeline[0].Command[0].SimpleCommand",
-			"            Word :",
-			"            List with 1 andOrs",
-			"           AndOr with 1 pipelines",
-			"        Pipeline with 1 commands",
-			"         Command ",
-			"     FunctionDef for fun",
-			" CompoundCommand ",
-			"            List with 1 andOrs",
-			"           AndOr with 1 pipelines",
-			"        Pipeline with 1 commands",
-			"         Command ",
-			"   SimpleCommand :",
-			"            Path List.AndOr[4].Pipeline[0].Command[0].CompoundCommand.Loop." +
-				"List[1].AndOr[0].Pipeline[0].Command[0].FunctionDefinition.CompoundCommand." +
-				"List.AndOr[0].Pipeline[0].Command[0].SimpleCommand",
-			"            Word :",
-			"       Redirects with 1 redirects",
-			"        Redirect >&",
-			"            Word 2"})
+		c.Check(commands, deepEquals, output)
 
 		// After parsing, there is not a single level of indentation,
 		// therefore even Parent(0) returns nil.
+		//
+		// This ensures that the w.push/w.pop calls are balanced.
 		c.Check(walker.Parent(0), equals, nil)
 	}
+
+	test(""+
+		"if condition; then action; else case selector in pattern) case-item-action ;; esac; fi; "+
+		"set -e; "+
+		"cd ${WRKSRC}/locale; "+
+		"for lang in *.po; do "+
+		"  [ \"$${lang}\" = \"wxstd.po\" ] && continue; "+
+		"  ${TOOLS_PATH.msgfmt} -c -o \"$${lang%.po}.mo\" \"$${lang}\"; "+
+		"done; "+
+		"while :; do fun() { :; } 1>&2; done",
+
+		"            List with 5 andOrs",
+		"           AndOr with 1 pipelines",
+		"        Pipeline with 1 commands",
+		"         Command ",
+		" CompoundCommand ",
+		"              If with 1 then-branches",
+		"            List with 1 andOrs",
+		"           AndOr with 1 pipelines",
+		"        Pipeline with 1 commands",
+		"         Command ",
+		"   SimpleCommand condition",
+		"            Path List.AndOr[0].Pipeline[0].Command[0].CompoundCommand.If.List[0].AndOr[0].Pipeline[0].Command[0].SimpleCommand",
+		"            Word condition",
+		"            List with 1 andOrs",
+		"           AndOr with 1 pipelines",
+		"        Pipeline with 1 commands",
+		"         Command ",
+		"   SimpleCommand action",
+		"            Path List.AndOr[0].Pipeline[0].Command[0].CompoundCommand.If.List[1].AndOr[0].Pipeline[0].Command[0].SimpleCommand",
+		"            Word action",
+		"            List with 1 andOrs",
+		"           AndOr with 1 pipelines",
+		"        Pipeline with 1 commands",
+		"         Command ",
+		" CompoundCommand ",
+		"            Case with 1 items",
+		"            Word selector",
+		"        CaseItem with 1 patterns",
+		"           Words with 1 words",
+		"            Word pattern",
+		"            List with 1 andOrs",
+		"           AndOr with 1 pipelines",
+		"        Pipeline with 1 commands",
+		"         Command ",
+		"   SimpleCommand case-item-action",
+		"            Path List.AndOr[0].Pipeline[0].Command[0].CompoundCommand.If."+
+			"List[2].AndOr[0].Pipeline[0].Command[0].CompoundCommand.Case.CaseItem[0]."+
+			"List.AndOr[0].Pipeline[0].Command[0].SimpleCommand",
+		"            Word case-item-action",
+		"           AndOr with 1 pipelines",
+		"        Pipeline with 1 commands",
+		"         Command ",
+		"   SimpleCommand set -e",
+		"            Path List.AndOr[1].Pipeline[0].Command[0].SimpleCommand",
+		"            Word set",
+		"           Words with 1 words",
+		"            Word -e",
+		"           AndOr with 1 pipelines",
+		"        Pipeline with 1 commands",
+		"         Command ",
+		"   SimpleCommand cd ${WRKSRC}/locale",
+		"            Path List.AndOr[2].Pipeline[0].Command[0].SimpleCommand",
+		"            Word cd",
+		"           Words with 1 words",
+		"            Word ${WRKSRC}/locale",
+		"           AndOr with 1 pipelines",
+		"        Pipeline with 1 commands",
+		"         Command ",
+		" CompoundCommand ",
+		"             For variable lang",
+		"         Varname lang",
+		"           Words with 1 words",
+		"            Word *.po",
+		"            List with 2 andOrs",
+		"           AndOr with 2 pipelines",
+		"        Pipeline with 1 commands",
+		"         Command ",
+		"   SimpleCommand [ \"$${lang}\" = \"wxstd.po\" ]",
+		"            Path List.AndOr[3].Pipeline[0].Command[0].CompoundCommand.For.List.AndOr[0].Pipeline[0].Command[0].SimpleCommand",
+		"            Word [",
+		"           Words with 4 words",
+		"            Word \"$${lang}\"",
+		"            Word =",
+		"            Word \"wxstd.po\"",
+		"            Word ]",
+		"        Pipeline with 1 commands",
+		"         Command ",
+		"   SimpleCommand continue",
+		"            Path List.AndOr[3].Pipeline[0].Command[0].CompoundCommand.For.List.AndOr[0].Pipeline[1].Command[0].SimpleCommand",
+		"            Word continue",
+		"           AndOr with 1 pipelines",
+		"        Pipeline with 1 commands",
+		"         Command ",
+		"   SimpleCommand ${TOOLS_PATH.msgfmt} -c -o \"$${lang%.po}.mo\" \"$${lang}\"",
+		"            Path List.AndOr[3].Pipeline[0].Command[0].CompoundCommand.For.List.AndOr[1].Pipeline[0].Command[0].SimpleCommand",
+		"            Word ${TOOLS_PATH.msgfmt}",
+		"           Words with 4 words",
+		"            Word -c",
+		"            Word -o",
+		"            Word \"$${lang%.po}.mo\"",
+		"            Word \"$${lang}\"",
+		"           AndOr with 1 pipelines",
+		"        Pipeline with 1 commands",
+		"         Command ",
+		" CompoundCommand ",
+		"            Loop ",
+		"            List with 1 andOrs",
+		"           AndOr with 1 pipelines",
+		"        Pipeline with 1 commands",
+		"         Command ",
+		"   SimpleCommand :",
+		"            Path List.AndOr[4].Pipeline[0].Command[0].CompoundCommand.Loop.List[0].AndOr[0].Pipeline[0].Command[0].SimpleCommand",
+		"            Word :",
+		"            List with 1 andOrs",
+		"           AndOr with 1 pipelines",
+		"        Pipeline with 1 commands",
+		"         Command ",
+		"     FunctionDef for fun",
+		" CompoundCommand ",
+		"            List with 1 andOrs",
+		"           AndOr with 1 pipelines",
+		"        Pipeline with 1 commands",
+		"         Command ",
+		"   SimpleCommand :",
+		"            Path List.AndOr[4].Pipeline[0].Command[0].CompoundCommand.Loop."+
+			"List[1].AndOr[0].Pipeline[0].Command[0].FunctionDefinition.CompoundCommand."+
+			"List.AndOr[0].Pipeline[0].Command[0].SimpleCommand",
+		"            Word :",
+		"       Redirects with 1 redirects",
+		"        Redirect >&",
+		"            Word 2")
 }
