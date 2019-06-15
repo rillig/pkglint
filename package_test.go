fc4219abf6c5b12f70fb04c3595a42151ade1986
Roland Illig <roland.illig@gmx.de>
2019-06-15 22:03:22 +0200

    Fix test for including Makefile.common from same package
    The test had been wrong all the time since the "used by" check only
    steps in if the Makefile.common is at least 3 lines long.

--- package_test.go
+++ package_test.go
@@ -1596,13 +1596,18 @@ func (s *Suite) Test_Package_parse__include_Makefile_common_same_directory(c *ch
 		".include \"../../category/dependency/Makefile.common\"",
 		".include \"Makefile.common\"")
 	t.CreateFileLines("category/package/Makefile.common",
-		MkRcsID)
+		MkRcsID,
+		"#",
+		"#")
 	t.FinishSetUp()
 
 	G.Check(t.File("category/package"))
 
 	t.CheckOutputLines(
-		"WARN: ~/category/dependency/Makefile.common:3: " +
+		"WARN: ~/category/dependency/Makefile.common:3: "+
+			"Please add a line \"# used by category/package/Makefile\" here.",
+		// FIXME: This line is unnecessary since the package Makefile is near enough.
+		"WARN: ~/category/package/Makefile.common:3: "+
 			"Please add a line \"# used by category/package/Makefile\" here.")
 }
 
