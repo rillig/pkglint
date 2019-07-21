d76bca92f17ea8daf3bd7ab92964722773fc0aa8
Roland Illig <roland.illig@gmx.de>
2019-07-21 21:37:24 +0200

    Fix warning about direct URL in GitHub MASTER_SITES

--- vartypecheck_test.go
+++ vartypecheck_test.go
@@ -490,8 +490,8 @@ func (s *Suite) Test_VartypeCheck_FetchURL(c *check.C) {
 
 	vt.Output(
 		"WARN: filename.mk:1: Please use ${MASTER_SITE_GITHUB:=example/} "+
-			"instead of \"https://github.com/example/project/\" "+
-			"and run \""+confMake+" help topic=github\" for further tips.",
+			"instead of \"https://github.com/example/\" "+
+			"and run \""+confMake+" help topic=github\" for further instructions.",
 		"WARN: filename.mk:2: Please use ${MASTER_SITE_GNU:=bison} "+
 			"instead of \"http://ftp.gnu.org/pub/gnu/bison\".",
 		"ERROR: filename.mk:3: The subdirectory in MASTER_SITE_GNU must end with a slash.",
@@ -529,10 +529,9 @@ func (s *Suite) Test_VartypeCheck_FetchURL(c *check.C) {
 	vt.Values(
 		"https://github.com/transmission/transmission-releases/raw/master/")
 	vt.Output(
-		// FIXME: the text after "instead of" is too long.
 		"WARN: filename.mk:51: Please use ${MASTER_SITE_GITHUB:=transmission/} " +
-			"instead of \"https://github.com/transmission/transmission-releases/raw/master/\" " +
-			"and run \"" + confMake + " help topic=github\" for further tips.")
+			"instead of \"https://github.com/transmission/\" " +
+			"and run \"" + confMake + " help topic=github\" for further instructions.")
 }
 
 func (s *Suite) Test_VartypeCheck_FetchURL__without_package(c *check.C) {
