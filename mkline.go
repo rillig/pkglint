f9cef3170e8dd2736709092c404d843c283ac379
Roland Illig <roland.illig@gmx.de>
2019-08-22 22:01:19 +0200

    In lists of conditional variables, mention each variable only once

--- mkline.go
+++ mkline.go
@@ -1372,16 +1372,16 @@ func (ind *Indentation) IsConditional() bool {
 //
 // Variables named *_MK are excluded since they are usually not interesting.
 func (ind *Indentation) Varnames() []string {
-	var varnames []string
+	varnames := NewStringSet()
 	for _, level := range ind.levels {
 		for _, levelVarname := range level.conditionalVars {
 			// multiple-inclusion guard must be filtered out earlier.
 			assert(!hasSuffix(levelVarname, "_MK"))
 
-			varnames = append(varnames, levelVarname)
+			varnames.Add(levelVarname)
 		}
 	}
-	return varnames
+	return varnames.Elements
 }
 
 // Args returns the arguments of the innermost .if, .elif or .for.
