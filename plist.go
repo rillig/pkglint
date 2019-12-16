bcb05052f25b628caea790a2c04d18d3af669617
Roland Illig <roland.illig@gmx.de>
2019-12-16 19:10:20 +0100

    Always trace debug information for PLIST sorting
    There was a branch missing in the code coverage. Once again it makes
    sense to just remove a condition to make the code more understandable.

--- plist.go
+++ plist.go
@@ -578,7 +578,7 @@ func NewPlistLineSorter(plines []*PlistLine) *plistLineSorter {
 
 func (s *plistLineSorter) Sort() {
 	if line := s.unsortable; line != nil {
-		if G.Logger.IsAutofix() && trace.Tracing {
+		if trace.Tracing {
 			trace.Stepf("%s: This line prevents pkglint from sorting the PLIST automatically.", line)
 		}
 		return
