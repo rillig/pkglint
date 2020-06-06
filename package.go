5b3a534fe2a36bc75dc996ebdd8570796fa33c5f
Roland Illig <roland.illig@gmx.de>
2020-06-06 07:51:18 +0200

    Extract distinfo file check into separate method

--- package.go
+++ package.go
@@ -660,39 +660,12 @@ func (pkg *Package) checkfilePackageMakefile(filename CurrPath, mklines *MkLines
 		defer trace.Call(filename)()
 	}
 
-	vars := pkg.vars
 	pkg.checkPlist()
 
-	want := true
-	if vars.IsDefined("NO_CHECKSUM") ||
-		vars.IsDefined("META_PACKAGE") ||
-		!vars.IsDefined("DISTNAME") ||
-		(vars.IsDefined("DISTFILES") && vars.LastValue("DISTFILES") == "") {
-		want = false
-	}
-	if !isEmptyDir(pkg.File(pkg.Patchdir)) || vars.IsDefined("DISTINFO_FILE") {
-		want = true
-	}
-
-	if !want {
-		distinfoFile := pkg.File(pkg.DistinfoFile)
-		if distinfoFile.IsFile() {
-			NewLineWhole(distinfoFile).Warnf("This file should not exist.")
-		}
-	} else {
-		distinfoFile := pkg.File(pkg.DistinfoFile)
-		if !containsVarUse(distinfoFile.String()) && !distinfoFile.IsFile() {
-			line := NewLineWhole(distinfoFile)
-			line.Warnf("A package that downloads files should have a distinfo file.")
-			line.Explain(
-				sprintf("To generate the distinfo file, run %q.", bmake("makesum")),
-				"",
-				"To mark the package as not needing a distinfo file, set",
-				"NO_CHECKSUM=yes in the package Makefile.")
-		}
-	}
+	pkg.checkDistinfoExists()
 
 	// TODO: There are other REPLACE_* variables which are probably also affected by NO_CONFIGURE.
+	vars := pkg.vars
 	if noConfigureLine := vars.FirstDefinition("NO_CONFIGURE"); noConfigureLine != nil {
 		if replacePerlLine := vars.FirstDefinition("REPLACE_PERL"); replacePerlLine != nil {
 			replacePerlLine.Warnf("REPLACE_PERL is ignored when NO_CONFIGURE is set (in %s).",
@@ -770,6 +743,39 @@ func (pkg *Package) checkfilePackageMakefile(filename CurrPath, mklines *MkLines
 	pkg.CheckVarorder(mklines)
 
 	SaveAutofixChanges(mklines.lines)
+}
+
+func (pkg *Package) checkDistinfoExists() {
+	vars := pkg.vars
+
+	want := true
+	if vars.IsDefined("NO_CHECKSUM") ||
+		vars.IsDefined("META_PACKAGE") ||
+		!vars.IsDefined("DISTNAME") ||
+		(vars.IsDefined("DISTFILES") && vars.LastValue("DISTFILES") == "") {
+		want = false
+	}
+	if !isEmptyDir(pkg.File(pkg.Patchdir)) || vars.IsDefined("DISTINFO_FILE") {
+		want = true
+	}
+
+	if !want {
+		distinfoFile := pkg.File(pkg.DistinfoFile)
+		if distinfoFile.IsFile() {
+			NewLineWhole(distinfoFile).Warnf("This file should not exist.")
+		}
+	} else {
+		distinfoFile := pkg.File(pkg.DistinfoFile)
+		if !containsVarUse(distinfoFile.String()) && !distinfoFile.IsFile() {
+			line := NewLineWhole(distinfoFile)
+			line.Warnf("A package that downloads files should have a distinfo file.")
+			line.Explain(
+				sprintf("To generate the distinfo file, run %q.", bmake("makesum")),
+				"",
+				"To mark the package as not needing a distinfo file, set",
+				"NO_CHECKSUM=yes in the package Makefile.")
+		}
+	}
 }
 
 // checkPlist checks whether the package needs a PLIST file,
