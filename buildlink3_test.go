ef7f9008105fe9e7b6b05d2dd4e72f00ba1b97f1
Roland Illig <roland.illig@gmx.de>
2021-05-25 23:42:10 +0200

    Demonstrate wrong warning for indirect PKGSRCDIR

--- buildlink3_test.go
+++ buildlink3_test.go
@@ -947,6 +947,27 @@ func (s *Suite) Test_Buildlink3Checker_checkVarassignPkgsrcdir(c *check.C) {
 			"not ../../category/other-package.")
 }
 
+func (s *Suite) Test_Buildlink3Checker_checkVarassignPkgsrcdir__var(c *check.C) {
+	t := s.Init(c)
+
+	t.SetUpPackage("category/package")
+	t.SetUpPackage("category/other-package")
+	t.CreateFileBuildlink3("category/package/buildlink3.mk",
+		"PACKAGE_SRCDIR=\t\t\t../../category/package",
+		"BUILDLINK_PKGSRCDIR.package?=\t${PACKAGE_SRCDIR}",
+		"BUILDLINK_API_DEPENDS.package+=\tpackage>=3")
+	t.FinishSetUp()
+
+	G.Check(t.File("category/package"))
+
+	// FIXME: Don't warn about indirect PKGSRCDIR.
+	t.CheckOutputLines(
+		"ERROR: ~/category/package/buildlink3.mk:13: " +
+			"BUILDLINK_PKGSRCDIR.package must be set " +
+			"to the package's own path (../../category/package), " +
+			"not ${PACKAGE_SRCDIR}.")
+}
+
 func (s *Suite) Test_Buildlink3Checker_checkVaruseInPkgbase__PKGBASE_with_variable_PHP_PKG_PREFIX(c *check.C) {
 	t := s.Init(c)
 
