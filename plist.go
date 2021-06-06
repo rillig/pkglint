89e5705a33d1fe26a1b702acb5c3542a648e61fc
Roland Illig <roland.illig@gmx.de>
2021-06-06 09:26:55 +0200

    Fix packages whose PLIST contains .egg-info directly
    An example package is biology/minimap2.
    
    Suggested by Brook Milligan via private mail.

--- plist.go
+++ plist.go
@@ -228,7 +228,17 @@ func (ck *PlistChecker) checkPathMisc(rel RelPath, pline *PlistLine) {
 			"since its contents depends on more than one package.")
 	}
 	if rel.ContainsText(".egg-info/") {
-		pline.Warnf("Include \"../../lang/python/egg.mk\" instead of listing .egg-info files directly.")
+		canFix := ck.pkg != nil && ck.pkg.CanFixAddInclude()
+		fix := pline.Autofix()
+		fix.Warnf("Include \"../../lang/python/egg.mk\" " +
+			"instead of listing .egg-info files directly.")
+		if canFix {
+			fix.Replace(pline.Path().Dir().String(), "${PYSITELIB}/${EGG_INFODIR}")
+		}
+		fix.Apply()
+		if canFix {
+			ck.pkg.FixAddInclude("../../lang/python/egg.mk")
+		}
 	}
 	if rel.ContainsPath("..") {
 		pline.Errorf("Paths in PLIST files must not contain \"..\".")
