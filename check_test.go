34b11947a53f34be27ac5f98a52431e2dc808089
Roland Illig <roland.illig@gmx.de>
2021-06-25 14:42:07 +0200

    Add tests for Logger and SeparatorWriter

--- check_test.go
+++ check_test.go
@@ -104,7 +104,6 @@ func (s *Suite) TearDownTest(c *check.C) {
 func Test__qa(t *testing.T) {
 	ck := intqa.NewQAChecker(t.Errorf)
 
-	ck.Configure("logging.go", "*", "*", -intqa.EMissingTest)        // TODO
 	ck.Configure("mkline.go", "*", "*", -intqa.EMissingTest)         // TODO
 	ck.Configure("mklineparser.go", "*", "*", -intqa.EMissingTest)   // TODO
 	ck.Configure("mklinechecker.go", "*", "*", -intqa.EMissingTest)  // TODO
