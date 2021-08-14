be8a1cc9cdb1a70e480932bf85c9726b4bee307a
Roland Illig <roland.illig@gmx.de>
2021-08-14 11:13:00 +0200

    Demonstrate wrong warning for man/man1 in INSTALLATION_DIRS

--- vartypecheck_test.go
+++ vartypecheck_test.go
@@ -1823,6 +1823,15 @@ func (s *Suite) Test_VartypeCheck_PrefixPathname(c *check.C) {
 			"since it is not relative to PREFIX.",
 		"ERROR: filename.mk:11: VARBASE must not be used in INSTALLATION_DIRS "+
 			"since it is not relative to PREFIX.")
+
+	vt.Varname("INSTALLATION_DIRS")
+	vt.Values(
+		"man/man1")
+
+	// FIXME: INSTALLATION_DIRS contains magic replacements.
+	vt.Output(
+		"WARN: filename.mk:21: " +
+			"Please use \"${PKGMANDIR}/man1\" instead of \"man/man1\".")
 }
 
 func (s *Suite) Test_VartypeCheck_PythonDependency(c *check.C) {
