677a851d402bb8187ec975795acaf79b778719fe
Roland Illig <roland.illig@gmx.de>
2023-09-02 03:00:34 +0200

    Add another test case for opsys-specific variables
    Seen in x11/XaoS/Makefile:22.

--- v23/mkexprchecker_test.go
+++ v23/mkexprchecker_test.go
@@ -1506,7 +1506,9 @@ func (s *Suite) Test_MkExprChecker_checkToolsPlatform(c *check.C) {
 		"CONFIGURE_ENV.SunOS+=\tA=${TOOLS_PLATFORM.cond1:Q}",
 		"CONFIGURE_ENV+=\t\tA=${TOOLS_PLATFORM.undefined:Q}",
 		"CONFIGURE_ENV.NetBSD+=\tA=${TOOLS_PLATFORM.undefined:Q}",
-		"CONFIGURE_ENV.SunOS+=\tA=${TOOLS_PLATFORM.undefined:Q}")
+		"CONFIGURE_ENV.SunOS+=\tA=${TOOLS_PLATFORM.undefined:Q}",
+		// TODO: warn
+		"CONFIGURE_ENV.any+=\tA=${TOOLS_PLATFORM.undefined:Q}")
 
 	mklines.Check()
 
